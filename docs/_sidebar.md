92bfb2fe14df72360d3a44b2cf2fb9e9361844b1
andyesfly <35127884@qq.com>
2019-10-29 11:34:41 +0800

    全文检索参数调整

--- docs/_sidebar.md
+++ docs/_sidebar.md
@@ -1,12 +1,10 @@
 - [快速上手](README.md)
 - 股票
 
-    - [基础数据](Stock-Base.md)
-    - [交易数据](Stock-Trade.md)
-    - [指数](Stock_Index.md)
-    - [财务数据](Stock-Finance.md)
+- [基础数据](Stock-Base.md)
+- [交易数据](Stock-Trade.md)
+- [指数](Stock_Index.md)
+- [财务数据](Stock-Finance.md)
 - 基金
 
-    - [基础数据](Fund-Base.md)
-    
-
+- [基础数据](Fund-Base.md)
--- test/test.js
+++ test/test.js
@@ -1,10 +1,45 @@
-const stock = require("../src/index");
+const dip = require("../src/index");
 
-// stock.symbols.getStockList().then((data)=>{
+// dip.stock.symbols.getStockList().then((data)=>{
 //     console.log(data);
 // });
 
-stock.trading.getMonthHis("sh600005").then((data)=>{
-    console.log(data);
-});
+// dip.stock.symbols.getBoards("600005").then((data)=>{
+//     console.log(JSON.stringify(data));
+// });
+
+// dip.stock.trading.getWeekHis("sh600005").then((data)=>{
+//     console.log(data);
+// });
+
+// dip.stock.trading.getDailyHis("17","sh000001").then((data)=>{
+//     console.log(JSON.stringify(data));
+// });
     
+// dip.stock.trading.getMin("sh600026").then((data)=>{
+//     console.log(JSON.stringify(data));
+// });
+// dip.stock.symbols.getStockIssue("000725").then((data)=>{
+//     console.log(JSON.stringify(data));
+// })
+// dip.stock.symbols.getIndustryList().then((data)=>{
+//     console.log(JSON.stringify(data));
+// })
+// dip.stock.symbols.getConceptList().then((data)=>{
+//     console.log(JSON.stringify(data));
+// })
+// dip.stock.symbols.getAreaList().then((data)=>{
+//     console.log(JSON.stringify(data));
+// })
+// dip.stock.index.getComponents("BK0447").then(data=>{
+//     console.log(JSON.stringify(data));
+// })
+// dip.stock.finance.getGuideLine("000651","2018").then(data=>{
+//     console.log(JSON.stringify(data));
+// })
+// dip.stock.finance.getBalanceSheet("000651","2019","general").then(data=>{
+//     console.log(JSON.stringify(data));
+// })
+dip.stock.finance.getCashFlowStatment("600030","2019","security").then(data=>{
+    console.log(JSON.stringify(data));
+})
