8133b028d5ff62737e847c5101d176a3c6a8de7b
andyesfly <35127884@qq.com>
2020-04-29 18:11:51 +0800

    📖 改用vuepress重新构建文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,44 @@
+// const { nav, sideBar } = require("vuepress-bar")();
+const autobar_options = {
+    stripNumbers: true,
+    maxLevel: 2,
+    navPrefix: "nav",
+    skipEmptySidebar: true,
+    skipEmptyNavbar: true,
+    multipleSideBar: true,
+    setHomepage: "hide" | "toGroup" | "top",
+    pinyinNav: true
+};
+
+module.exports = {
+    title: "dipiper",
+    description: "一个基于 nodejs 的开源的财经数据接口包",
+    head: [
+        [
+            "link",
+            {
+                rel: "icon",
+                href: `./favicon.ico`
+            }
+        ]
+    ],
+    dest: "./docs/.vuepress/dist",
+    plugins: ["permalink-pinyin", "rpurl", ["autobar", autobar_options]],
+    theme: "reco",
+    themeConfig: {
+        author: "嘟嘟爸爸",
+        logo: "logo.png",
+        mode: "light",
+        modePicker: false,
+        nav: [
+            { text: "首页", link: "/" },
+            { text: "文档", link: "/zhi-nan/" }
+        ],
+        displayAllHeaders: true,
+        record: "滇ICP备16007768号",
+        recordLink: "http://www.beian.miit.gov.cn",
+        repo: "https://github.com/andyesfly/dipiper",
+        repoLabel: "My Github",
+        startYear: "2018"
+    }
+};
---
+++ docs/01.指南/README.md
@@ -0,0 +1,26 @@
+# 快速上手
+
+这是一个基于 nodejs 的开源的财经数据接口包，为量化投资提供数据来源，满足金融量化分析师和学习数据分析的人在数据获取方面的需求。
+
+从数据采集到数据可视化都用同一种开发语言——javascript，降低了学习成本，让我们专注于分析模型的研究。
+
+本接口包采用 puppeteer 作为爬虫内核，在安装的时候需要下载 Chromium ，国内请使用淘宝镜像，否则会被墙，导致 puppeteer 无法安装成功
+
+### 安装
+
+`cnpm install dipiper --save`
+
+### 使用方法
+
+```javascript
+const dip = require("dipiper");
+
+dip.stock.symbols.getStockList().then((data) => {
+    //数据存储、处理逻辑，请自行实现
+    console.log(data);
+});
+```
+
+### 交流反馈
+
+QQ 群：383331051
